e1db3af8ef3db683a2ad9b9527e1015ea3606ec6
LiNk-NY <marcel.ramos@sph.cuny.edu>
2025-06-09 16:17:58 -0400

    update app.R

--- app.R
+++ app.R
@@ -1,3 +1,11 @@
 # Launch the ShinyApp (Do not remove this comment)
 
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+options(repos = BiocManager::repositories())
+
+if (!requireNamespace("BiocPkgDash", quietly = TRUE))
+    BiocManager::install("Bioconductor/BiocPkgDash")
+
 BiocPkgDash::BiocPkgDash() # add parameters here (if any)
