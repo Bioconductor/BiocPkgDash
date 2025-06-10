24f203fabccac710b61706742319c0f91504f155
LiNk-NY <marcel.ramos@sph.cuny.edu>
2025-06-10 17:49:23 -0400

    add value boxes to badges tab

--- R/BiocPkgDash.R
+++ R/BiocPkgDash.R
@@ -50,7 +50,35 @@ BiocPkgDash <- function(...) {
                 tabsetPanel(
                     tabPanel(
                         "Badges",
-                        badgesUI("badges1")
+                        fluidRow(
+                            column(
+                                width = 9,
+                                badgesUI("badges1")
+                            ),
+                            column(
+                                width = 3,
+                                bslib::value_box(
+                                    title = "Total Packages",
+                                    value = textOutput("pkg_count"),
+                                    showcase = bsicons::bs_icon("hash"),
+                                    theme = "primary"
+                                ),
+                                bslib::value_box(
+                                    title = "Year-To-Date Downloads",
+                                    value = textOutput("dl_count"),
+                                    showcase = bsicons::bs_icon(
+                                        "cloud-download"
+                                    ),
+                                    theme = "secondary"
+                                ),
+                                bslib::value_box(
+                                    title = "Total No. of Dependencies",
+                                    value = textOutput("dep_count"),
+                                    showcase = bsicons::bs_icon("collection"),
+                                    theme = "warning"
+                                )
+                            )
+                        )
                     ),
                     tabPanel(
                         "Status",
@@ -120,6 +148,51 @@ BiocPkgDash <- function(...) {
                     )
                     return(result)
                 }
+            )
+        })
+
+        output$pkg_count <- renderText({
+            nrow(maintainedData())
+        })
+
+        output$dl_count <- renderText({
+            all_dls <- vapply(
+                maintainedData()[["Package"]],
+                function(pkg) {
+                    dl_pkg <- suppressWarnings({
+                        BiocPkgTools::pkgDownloadStats(pkg)
+                    })
+                    dls <- sum(dl_pkg[["Nb_of_distinct_IPs"]])
+                    if (!length(dls))
+                        0
+                    else
+                        dls
+                },
+                numeric(1L)
+            )
+            prettyNum(
+                sum(all_dls),
+                big.mark = ",",
+                scientific = FALSE
+            )
+        })
+
+        output$dep_count <- renderText({
+            total_deps <- vapply(
+                maintainedData()[["Package"]],
+                function(pkg) {
+                    deps <- BiocPkgTools::pkgBiocDeps(
+                        pkg,
+                        which = "all"
+                    )
+                    length(unlist(deps))
+                },
+                numeric(1L)
+            )
+            prettyNum(
+                sum(total_deps),
+                big.mark = ",",
+                scientific = FALSE
             )
         })
 
