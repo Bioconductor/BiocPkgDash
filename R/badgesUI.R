d09bd4d6ed0c675e48f8a601d6dbf6f17da24cfd
LiNk-NY <marcel.ramos@sph.cuny.edu>
2025-06-11 14:23:35 -0400

    add shinyccsloader

--- R/badgesUI.R
+++ R/badgesUI.R
@@ -1,6 +1,10 @@
 badgesUI <- function(id, label = "badges") {
     ns <- NS(id)
     tagList(
-        DT::dataTableOutput(ns("badge_out"))
+        shinycssloaders::withSpinner(
+            DT::dataTableOutput(ns("badge_out")),
+            type = 5,
+            color = "#1a81c2"
+        )
     )
 }
