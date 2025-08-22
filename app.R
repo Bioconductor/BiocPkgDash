92397ce4ec2f72846865a53a2870b61b3428499d
LiNk-NY <marcel.ramos@sph.cuny.edu>
2025-08-22 12:37:34 -0400

    update shinybiocloader location

--- app.R
+++ app.R
@@ -9,7 +9,7 @@ if (!requireNamespace("remotes", quietly = TRUE))
 options(repos = BiocManager::repositories())
 
 if (!requireNamespace("shinybiocloader", quietly = TRUE))
-    BiocManager::install("LiNk-NY/shinybiocloader")
+    BiocManager::install("Bioconductor/shinybiocloader")
 
 if (!requireNamespace("BiocPkgDash", quietly = TRUE))
     BiocManager::install("Bioconductor/BiocPkgDash")
